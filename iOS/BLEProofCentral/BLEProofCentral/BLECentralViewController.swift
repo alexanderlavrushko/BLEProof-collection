dcf98150b3dd7e12fd48fa36058965f822c12106
Alexander Lavrushko <lavrushko92@gmail.com>
2021-03-22 01:01:58 +0100

    iOS: Central: Add full UI without handling

--- iOS/BLEProofCentral/BLEProofCentral/BLECentralViewController.swift
+++ iOS/BLEProofCentral/BLEProofCentral/BLECentralViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class BLECentralViewController: UIViewController {
 
+    @IBOutlet weak var textViewStatus: UITextView!
+    @IBOutlet weak var textViewLog: UITextView!
+    @IBOutlet weak var switchConnect: UISwitch!
+    @IBOutlet weak var textFieldDataForRead: UITextField!
+    @IBOutlet weak var textFieldDataForWrite: UITextField!
+    @IBOutlet weak var textFieldDataForIndicate: UITextField!
+    @IBOutlet weak var labelSubscription: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        textViewStatus.layer.borderWidth = 1
+        textViewStatus.layer.borderColor = UIColor.gray.cgColor
+        textViewLog.layer.borderWidth = 1
+        textViewLog.layer.borderColor = UIColor.gray.cgColor
+
+        textFieldDataForRead.isUserInteractionEnabled = false
+        textFieldDataForIndicate.isUserInteractionEnabled = false
+
+        // text field delegate will hide keyboard after "return" is pressed
+        textFieldDataForRead.delegate = self
+        textFieldDataForWrite.delegate = self
+        textFieldDataForIndicate.delegate = self
     }
 
+    @IBAction func onChangeSwitchConnect(_ sender: UISwitch) {
+    }
 
+    @IBAction func onTapReadCharacteristic(_ sender: Any) {
+    }
+
+    @IBAction func onTapWriteCharacteristic(_ sender: Any) {
+    }
+
+    @IBAction func onTapOpenSettings(_ sender: Any) {
+    }
+
+    @IBAction func onTapClearLog(_ sender: Any) {
+    }
 }
 
+// MARK: - UI related methods
+extension BLECentralViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
