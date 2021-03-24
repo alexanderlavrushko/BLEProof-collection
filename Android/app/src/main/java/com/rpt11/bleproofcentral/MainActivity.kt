c11e45afac40900df9e35cfc386e8f96968fd4a6
Alexander Lavrushko <lavrushko92@gmail.com>
2021-03-24 20:35:03 +0100

    Android: Central: Add full UI without handling

--- Android/app/src/main/java/com/rpt11/bleproofcentral/MainActivity.kt
+++ Android/app/src/main/java/com/rpt11/bleproofcentral/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.rpt11.bleproofcentral
 
+import android.bluetooth.BluetoothGattCharacteristic
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onTapRead(view: View) {
+    }
+
+    fun onTapWrite(view: View) {
+    }
+
+    fun onTapClearLog(view: View) {
     }
 }
