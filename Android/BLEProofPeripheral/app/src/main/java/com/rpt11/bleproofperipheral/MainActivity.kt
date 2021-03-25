97bb7bad10ddc394b2db9251f5795d9fe5d7ab2e
Alexander Lavrushko <lavrushko92@gmail.com>
2021-03-25 22:08:41 +0100

    Android: Peripheral: Add full UI without handling

--- Android/BLEProofPeripheral/app/src/main/java/com/rpt11/bleproofperipheral/MainActivity.kt
+++ Android/BLEProofPeripheral/app/src/main/java/com/rpt11/bleproofperipheral/MainActivity.kt
@@ -2,10 +2,17 @@ package com.rpt11.bleproofperipheral
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onTapSend(view: View) {
+    }
+
+    fun onTapClearLog(view: View) {
     }
 }
