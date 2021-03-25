40433e4ed44dd7eb2ef53c510af8292479f3dfee
Alexander Lavrushko <lavrushko92@gmail.com>
2021-03-25 21:59:17 +0100

    Android: Peripheral: Add empty project (autogenerated)

---
+++ Android/BLEProofPeripheral/app/src/main/java/com/rpt11/bleproofperipheral/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rpt11.bleproofperipheral
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
